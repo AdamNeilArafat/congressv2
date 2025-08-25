74c7a130f43d3799f6510fc959eb054aa5e1a190
AdamNeilArafat <adam.n.arafat@gmail.com>
2025-08-25 07:39:42 -0700

    Upload FEC/Contriub from commitee to candid or indep exp/pas224/rejoin.cmd 2025-08-25 07:39:31)

---
+++ FEC/Contriub from commitee to candid or indep exp/pas224/rejoin.cmd
@@ -0,0 +1,11 @@
+@echo off
+cd /d "%~dp0"
+echo Rejoining itpas24.txt...
+copy /b "itpas24.txt.part001" + "itpas24.txt.part002" "itpas24.txt.rejoined.txt" >nul
+for %%P in ("itpas24.txt.part0??" "itpas24.txt.part1??") do (
+  if /I not "%%~nxP"=="itpas24.txt.part001" if /I not "%%~nxP"=="itpas24.txt.part002" (
+    copy /b "itpas24.txt.rejoined.txt"+"%%~nxP" "itpas24.txt.rejoined.txt" >nul
+  )
+)
+echo Done. Output: itpas24.txt.rejoined.txt
+pause
